d20a726373c5a5958c474a03046d53cc19b78b2d
LukFil <lukas.p.filipcik@gmail.com>
2024-02-20 14:10:59 +0100

    fix(components): carousel next/prev ensure arrow is centered

--- apps/www/registry/new-york/ui/carousel.tsx
+++ apps/www/registry/new-york/ui/carousel.tsx
@@ -216,7 +216,7 @@ const CarouselPrevious = React.forwardRef<
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeftIcon className="h-4 w-4" />
+      <ArrowLeftIcon className="mx-auto h-4 w-4" />
       <span className="sr-only">Previous slide</span>
     </Button>
   )
